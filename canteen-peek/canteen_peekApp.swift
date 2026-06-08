1cd28fd13139c816868e4fdd793a1e7641bd64fa
Azziz Abdul Ghofur <152408235+Azzizag@users.noreply.github.com>
2026-06-08 13:09:09 +0700

    Initial Commit

---
+++ canteen-peek/canteen_peekApp.swift
@@ -0,0 +1,17 @@
+//
+//  canteen_peekApp.swift
+//  canteen-peek
+//
+//  Created by Azziz Abdul Ghofur on 08/06/26.
+//
+
+import SwiftUI
+
+@main
+struct canteen_peekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
